c3779aa26fe917e4d7581504c88a3828747aa201
thks01 <88830255+thks01@users.noreply.github.com>
2022-11-12 15:50:18 +0900

    test1

--- server.js
+++ server.js
@@ -129,8 +129,20 @@ app.post("/survey", (req, res) => {
   const body = req.body;
   connection.query(
     "UPDATE sys.users SET age = ?, gender = ?, eduBackground = ?, degree = ? WHERE id = ?",
-    [body.age, body.gender, body.eduBackground, body.degree, body.id]
-  );
+    [body.age, body.gender, body.eduBackground, body.degree, body.id],
+    (err, result) => {
+      if (err) {
+        console.log(err)
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        console.log(result)
+        res.send(result);
+      } else {
+        console.log(result)
+        res.status(404).send({ message: "실패" });
+      }
+  ), ;
 });
 
 app.post("/findpassword", (req, res) => {
